e03485e5adc0bb40fff9c083e4544998edc20202
crispizza <hakgeumkim@gmail.com>
2018-03-04 12:00:56 +0900

    Sound Manager and Edit Button

---
+++ town/Assets/scripts/EditButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EditButton : MonoBehaviour {
+
+    private GameSystem gameSystem;
+    private SoundManager soundManager;
+
+    public void Awake()
+    {
+        gameSystem = GameObject.Find("GameSystems").GetComponent<GameSystem>();
+        soundManager = GameObject.Find("Sound Manager").GetComponent<SoundManager>();
+    }
+
+    public void EditButtonDown()
+    {
+        soundManager.Play(soundManager.sound[0]);
+
+        if(gameSystem.gameState_editable == 0)
+            gameSystem.EditMode(true);
+        else
+            gameSystem.EditMode(false);
+
+    }
+}
---
+++ town/Assets/scripts/SoundManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+    public AudioClip[] sound;
+    
+    public void Play(AudioClip sound)
+    {
+        gameObject.GetComponent<AudioSource>().PlayOneShot(sound);
+        
+    }
+
+
+}
