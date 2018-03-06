c40160695b38abc8b8d68b50554d7ae8ebc59d37
crispizza <hakgeumkim@gmail.com>
2018-03-06 14:43:12 +0900

    BGM Singleton

---
+++ town/Assets/scripts/BGMManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMManager : MonoBehaviour {
+
+    private static BGMManager instance = null;
+    public static BGMManager Instance
+    {
+        get { return instance; }
+    }
+
+    void Awake()
+    {
+        if (instance !=null && instance !=this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+}
