1567b075f3972b34413150bdceacbd1ef813763e
crispizza <hakgeumkim@gmail.com>
2018-03-08 20:22:27 +0900

    android patch -laptop

---
+++ town/Assets/scripts/AndroidManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AndroidManager : MonoBehaviour {
+    private static AndroidManager instance = null;
+    public static AndroidManager Instance
+    {
+        get { return instance; }
+    }
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Update()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                
+                //Application.Quit();
+            }
+        
+    }
+    
+
+}
